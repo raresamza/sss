6a72a112c363d2c0247c663a028537b6c72c32d9
Rares <rares.amza01@e-uvt.ro>
2023-05-26 14:51:46 +0300

    deleted style from textarea

--- src/components/QuizPage.js
+++ src/components/QuizPage.js
@@ -53,7 +53,7 @@ const QuizPage = () => {
                 <textarea spellCheck={false} readOnly={true} className='mt-40 bg-black  w-full rounded-lg decoration-none border-none outline-none h-[800px] resize-none text-white px-4 py-4'></textarea>
                 <h1 className='font-semibold pb-4'>Solution</h1>
                 <div className='grid grid-cols-1 grid-rows mb-10'>
-                    <textarea spellCheck={false} className='overflow-hidden bg-black  w-full rounded-t-lg decoration-none border-none outline-none h-[800px] resize-none text-white px-4 py-4'></textarea>
+                    <textarea spellCheck={false} className=' bg-black  w-full rounded-t-lg decoration-none border-none outline-none h-[800px] resize-none text-white px-4 py-4'></textarea>
                     <div className='bg-gray-800 h-16 px-4 rounded-b-lg flex justify-end items-center  '>
                         <button className="bg-green-600 rounded-lg h-12  w-36 align-left text-white float-right hover:bg-green-700 "><span className="text-lg mr-3 ">Run</span> <FontAwesomeIcon icon={faPlay} /></button>
                     </div>
